b4714e24f8cb3b901dd22d9720167be0f118cc5d
Hubert <h.smiechowicz@ideo.pl>
2024-05-27 15:24:53 +0200

    Base module configuration

---
+++ src/Modules/Base/Calcis.Modules.Base.Api/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calcis.Modules.Base.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController : ControllerBase
+    {
+        public BaseController() { }
+
+        [HttpGet]
+        public ActionResult<string> Hello()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ src/Modules/Base/Calcis.Modules.Base.Api/Extentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcis.Modules.Base.Api
+{
+    public static class Extentions
+    {
+        public static IServiceCollection AddBaseModule(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
