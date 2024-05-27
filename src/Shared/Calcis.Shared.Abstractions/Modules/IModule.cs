9c9a8030314f54dd4ff5349b2d99aa7c28d694da
Hubert <h.smiechowicz@ideo.pl>
2024-05-27 15:48:13 +0200

    Prepare for change modularity infrastructure

---
+++ src/Shared/Calcis.Shared.Abstractions/Modules/IModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcis.Shared.Abstractions.Modules
+{
+    public interface IModule
+    {
+        string Name { get; }
+        void Register(IServiceCollection service);
+    }
+}
